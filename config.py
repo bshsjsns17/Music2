7f489ab3cee0b4cc0b744d139de68c0678fdc74b
hshsbsb72 <105228187+hshsbsb72@users.noreply.github.com>
2022-05-18 01:59:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,11 @@ import os
 from dotenv import load_dotenv
 from pyrogram import Client, filters
 from pytgcalls import PyTgCalls
+"""
+API_ID 11798215
+API_HASH 433b9d1c8d24b23e0101cfd42ce262e8
+"""
+
 # للنشر المحلي
 if os.path.exists(".env"):
     load_dotenv(".env")
